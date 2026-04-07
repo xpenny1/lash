36ba97b12d7769f8c99abc56e848909d8ecc0d76
Can Ward <can.ward.99@gmail.com>
2026-04-07 08:53:22 +0200

    merge with local

--- bootstrap/parse.c
+++ bootstrap/parse.c
@@ -33,12 +33,17 @@ char *manyP(char *str, char *parser(char *)) {
     return str;
 }
 
-char *manyOneP(char *str, char *parser(char *)) {
-    if (!parser(str)) return NULL;
-    return manyP(str, parser);
+typedef char *(*Parser)(char *);
+char *many(char *str, Parser parser) {
+	char *tmp;
+	while ((tmp=parser(str))) str++; 
+	return str;
 }
-
-char *whitespaceP(char *str) {
+char *manyOne(char *str, Parser parser) {
+    	if (!(str=parser(str))) return NULL;
+    	return many(str, parser);
+}
+char *whitespaceCharP(char *str) {
 	switch (*str) {
 		case ' ':  return ++str; 
 		case '\t': return ++str; 
@@ -77,17 +82,23 @@ char *charP(char *str) {
 	if (*str++ != 'r') return NULL; 
 	return str;
 }
-
 char *typeP(char *str, Type *type) {
-	char *tmp = NULL;
+	char *tmp;
 	type->pinterLevel = 0;
 	if      ((tmp=charP(str))) {type->primitiveType=Char;str=tmp;}
 	else if ((tmp=intP(str)))  {type->primitiveType=Int; str=tmp;}
 	else                       {return NULL;}
-	while ((tmp=whitespaceP(str))) str=tmp;
+	str=many(str, whitespaceCharP);
 	while (*str == '*') {type->pinterLevel++;str++;}
 	type->arrayLevel = 0;
 	return str;
+}
+char *argP(char *str, Arg *arg) {
+    if (!(str=typeP(str, &arg->type))) return NULL;
+    arg->name.start=str;
+	    if (!(str=manyOne(str, letterP)))  return NULL;
+    arg->name.end=str;
+    return str;
 }
 
 char *hashBraceOpen(char *str, int *hashesPtr) {
@@ -105,46 +116,51 @@ char *hashBraceClose(char *str, int hashes) {
 
 char *funcP(char *str, Func *func){
 	char *tmp;
+	str=many(str, whitespaceCharP); 
 	if (*str++ != 'f') return NULL; 
 	if (*str++ != 'u') return NULL; 
 	if (*str++ != 'n') return NULL; 
 	if (*str++ != 'c') return NULL; 
-	if (!(str=manyP(str,whitespaceP))) return NULL;
-	func->name.start = str;
-	if (!(str=manyOneP(str, letterP))) return NULL;
-	func->name.end = str;
-	if (!(str=manyP(str,whitespaceP))) return NULL;
-
+	if (!(str=manyOne(str, whitespaceCharP))) return NULL;
+	name->start = str;
+		while ((tmp = letterP(str))) str=tmp;
+	name->end = str;
+	if (!(str=manyOne(str, whitespaceCharP))) return NULL;
 	if (*str++ != '(') return NULL;
-	if (!(str=manyP(str,whitespaceP))) return NULL;
-	int argIndex = 0;
+	int argCounter = 	0;
 	while (*str != ')') {
-		while ((tmp = whitespaceP(str))) {str=tmp;}
-		if (!(str = typeP(str, &(func->args[argIndex].type)))) return NULL;
-		func->args[argIndex].name.start=str;
-		if (!(str = wordP(str))) return NULL;
-		func->args[argIndex].name.end=str;
-		if (!(str=manyP(str,whitespaceP))) return NULL;
-		argIndex++;
-		switch (*str) {
-			case ',': str++; break;
-			case ')': break;
-			default: return NULL;
-		}
+    		if (argCounter!=0 && *str++!=',') return NULL;
+		str=many(str, whitespaceCharP); 
+		if (!(str = argP(str, &args[argCounter]))) return NULL;
+		str=many(str, whitespaceCharP); 
+		argCounter+=1;
 	}
-	func->argc = argIndex;
 	str++;
-	str=manyP(str, whitespaceP);
-	if (*str++ != '-') return NULL;
+	str=many(str, whitespaceCharP); 
+	if (*str++ != '-') return NULL; 
 	if (*str++ != '>') return NULL;
-	str=manyP(str, whitespaceP);
-	if(!(str=typeP(str, &func->ret))) return NULL; 
-	int hashes;
-	if (!(str=hashBraceOpen(str, &hashes))) return NULL;
-	func->code.start = str;
-	while(!(tmp=hashBraceClose(str++, hashes))) if (!*str) return NULL;
-	func->code.end = str-1;
-	str = tmp;
+	str=many(str, whitespaceCharP);
+	if (!(str=typeP(str, type))) return NULL;
+	str=many(str, whitespaceCharP);
+	code->start=str+1;
+	if (*str++ != '{') return NULL;
+	int braceLevel = 1;
+	while (braceLevel) {
+    		switch (*str++) {
+    			case '{': braceLevel++;
+    				break;
+    			case '}': braceLevel--;
+    				break;
+    			default:
+        			break;
+    		}
+	}
+	code->end=str-1;
+	str=many(str, whitespaceCharP); 
+
+	//if (!(str = wordP(str))) return NULL;
+	//while ((tmp = whitespace(str))) {str=tmp;}
+
 	return str;
 }
 
@@ -155,7 +171,7 @@ void printSV(StringView sv) {
 char svBuffer[1024] = {0};
 char *sPrintView(StringView sv) {
 	memcpy(svBuffer, sv.start, sv.end - sv.start);
-	svBuffer[1 + sv.end - sv.start] = 0;
+	svBuffer[sv.end - sv.start] = 0;
 	return svBuffer;
 }
 
@@ -176,27 +192,32 @@ void printArg(Arg arg) {
 
 char buffer[1024] = {0};
 int main(int argc, char **args) {
+	char buffer[1024] = {0};
 	char *input = buffer;
 	fread(input, sizeof(char), 1024, stdin); 
 	printf("%s\n", input); 
 
+	StringView name = {0};
 	Arg funcArgs[100] = {0};
-	Func func = {0};
-	func.args = funcArgs;
+	Type type = {0};
+	StringView code = {0};
 
-	if ((input = funcP(input, &func))) {
+	if ((input = funcP(input, &name, funcArgs, &type, &code))) {
 		printf("Func!\n");  
 		printf("%s\n", input);
-
-		printf("Name: %s\n", sPrintView(func.name));
-		printf("Argc: %d\n", func.argc);
-		printf("Args\n");
-		for (int i=0; i<func.argc; i++) {
-			printArg(func.args[i]); 
+		//Arg *arg;
+		printf("###Name: %s\n", sPrintView(name)); 
+		int argCounter = 0;
+		Arg arg;
+		while ((arg=funcArgs[argCounter]).name.start) {
+			printf("###Arg %d\n", argCounter);
+			printArg(funcArgs[argCounter]); 
+			argCounter++;
 		}
-		printf("Ret\n");
-		printType(func.ret); 
-		printf("%s\n",sPrintView(func.code));
+		printf("###Returns\n");
+		printType(type); 
+		printf("###Code\n");
+		printSV(code);
         } else {
 		printf("No Func!\n");  
 	}
